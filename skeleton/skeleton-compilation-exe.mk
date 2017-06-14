181951bdb5321aff03a50a18df4b69e4a0ebe0d1
Jean-Michel Paris <jean_michel_paris@hotmail.fr>
2017-06-14 23:57:44 +0200

    remove old compilation exe file

--- skeleton/skeleton-compilation-exe.mk
+++
@@ -1,8 +0,0 @@
- include ${MKMK_HOME}/rules/rule-$(LANG)-SUFFIXES.mk
--include ${MKMK_HOME}/vars/var-*.mk
- include ${MKMK_HOME}/rules/rule-$(LANG)-CORE.mk
- include ${MKMK_HOME}/rules/rule-clean.mk
- include ${MKMK_HOME}/rules/rule-install-uninstall.mk
-#UPDATE_URL = 'https://raw.githubusercontent.com/jmparis/Makefiles/master/mk/rules/skeleton-compilation-exe.mk'
-#include ./.mk/rules/rule-update.mk
- include ${MKMK_HOME}/rules/rule-DEFAULT.mk
