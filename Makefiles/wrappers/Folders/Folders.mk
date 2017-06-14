328bc3413b4a6b101fc0628a9ec6db8af525531b
Jean-Michel Paris <jean_michel_paris@hotmail.fr>
2017-06-15 00:48:31 +0200

    simplified wrappers

---
+++ Makefiles/wrappers/Folders/Folders.mk
@@ -0,0 +1,12 @@
+TOPTARGETS	:=	all clean clobber install uninstall help update
+SUBDIRS		:=	$(wildcard */.)
+
+.PHONY:	$(TOPTARGETS) $(SUBDIRS)
+$(TOPTARGETS):	$(SUBDIRS)
+$(SUBDIRS):
+	@printf %.$(MAKELEVEL)b%b " " "\033[1;34m[\033[1;35m$@\033[1;34m]\033[0m\n"
+	@$(MAKE) --no-print-directory -C $@ $(MAKECMDGOALS)
+
+#UPDATE_URL = 'https://raw.githubusercontent.com/jmparis/Makefiles/master/Makefile-folders.mk'
+#include ./.mk/rules/rule-update.mk
+include ./.mk/rules/rule-DEFAULT.mk
