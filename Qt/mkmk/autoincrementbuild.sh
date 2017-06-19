bba06a9a775b4a24ce2628199642c641fdfc0401
Jean-Michel Paris <jean_michel_paris@hotmail.fr>
2017-06-19 22:40:49 +0200

    filter \r generated in build_number

--- Qt/mkmk/autoincrementbuild.sh
+++ Qt/mkmk/autoincrementbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-number=`cat build_number`
+number=`cat build_number | tr -d '\r'`
 number=`expr $number + 1`
 echo "$number" | tee build_number
 #<-- output and save the number back to file
