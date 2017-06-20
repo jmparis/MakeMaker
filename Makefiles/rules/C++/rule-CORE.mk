f04863f5f51464457ac1b4f54a716b44b15b148b
Jean-Michel Paris <jean_michel_paris@hotmail.fr>
2017-06-21 01:54:17 +0200

    Fix indentation onto recursion

--- Makefiles/rules/C++/rule-CORE.mk
+++ Makefiles/rules/C++/rule-CORE.mk
@@ -15,38 +15,37 @@ DEPEND.cpp = $(CXX)  $(DEPFLAGS) $(CXXFLAGS) $(CPPFLAGS) $(TARGET_ARCH)
 
 ###
 
-PRT_TAB		=	%$(shell echo \($(MAKELEVEL) + 1 \) \* 2 | bc)b%b
+PRT_TAB		:=	%$(shell echo \($(MAKELEVEL) + 1 \) \* 2 | bc)b%b
 
 ###
 
-LINK_CPP_0		=	@printf "Linking.....: $(BINDIR)/\033[1;31m$(EXE)\033[0m\n"; printf $(PRT_TAB) " " ""; $(LINK.cpp)
+LINK_CPP_0		=	@printf "Linking.....: $(BINDIR)/\033[1;31m$(EXE)\033[0m\n"; $(LINK.cpp)
 LINK_CPP_1		=	$(LINK.cpp)
 LINK_CPP		=	$(LINK_CPP_$(V))
 
-$(BINDIR)/$(EXE):	$(OBJFILES) | $(BINDIR) first_tab
+$(BINDIR)/$(EXE):	$(OBJFILES) | $(BINDIR)
+	@printf "\r"
+	@printf $(PRT_TAB) " " ""
 	$(LINK_CPP) $^ $(LOADLIBES) $(LDLIBS) -o $@
 
 ###
 
-COMPILE_CPP_0	=	@printf "Compiling...: $(OBJDIR)/\033[1;35m$(notdir $@)\033[0m\n"; printf $(PRT_TAB) " " ""; $(COMPILE.cpp)
+COMPILE_CPP_0	=	@printf "Compiling...: $(OBJDIR)/\033[1;35m$(notdir $@)\033[0m\n"; $(COMPILE.cpp)
 COMPILE_CPP_1	=	$(COMPILE.cpp)
 COMPILE_CPP		=	$(COMPILE_CPP_$(V))
 
-DEPEND_CPP_0	=	@printf "Dependancies: $(DEPDIR)/\033[1;34m$*.d\033[0m\n"; printf $(PRT_TAB) " " ""; $(DEPEND.cpp)
+DEPEND_CPP_0	=	@printf "Dependancies: $(DEPDIR)/\033[1;34m$*.d\033[0m\n"; $(DEPEND.cpp)
 DEPEND_CPP_1	=	$(DEPEND.cpp)
 DEPEND_CPP		=	$(DEPEND_CPP_$(V))
 
-$(OBJDIR)/%.o:	$(SRCDIR)/%.cpp | $(OBJDIR) $(DEPDIR) first_tab
+$(OBJDIR)/%.o:	$(SRCDIR)/%.cpp | $(OBJDIR) $(DEPDIR)
+	@printf "\r"
+	@printf $(PRT_TAB) " " ""
 	$(COMPILE_CPP)	$(OUTPUT_OPTION) $<
+	@printf $(PRT_TAB) " " ""
 	$(DEPEND_CPP)	$< > $(DEPDIR)/$*.d
 
 -include $(DEPFILES)
-
-.PHONY: first_tab
-first_tab:
-ifeq	($(MAKELEVEL), 0)
-	@printf $(PRT_TAB) " " ""
-endif
 
 $(OBJDIR):
 	@mkdir	$(OBJDIR)
--- Makefiles/rules/Folders/rule-Folders.mk
+++ Makefiles/rules/Folders/rule-Folders.mk
@@ -1,16 +1,19 @@
 TOPTARGETS	:=	all clean clobber install uninstall run help update
 SUBDIRS		:=	$(sort $(wildcard */.))
 
-PRT_TAB		=	%$(shell echo \($(MAKELEVEL) + 1 \) \* 2 | bc)b%b
+PRT_TAB		:=	%$(shell echo \($(MAKELEVEL) + 1 \) \* 2 | bc)b%b
 
 .PHONY:	$(TOPTARGETS) $(SUBDIRS)
 $(TOPTARGETS):	$(SUBDIRS)
 $(SUBDIRS):
+	@printf "\r"
 	@printf $(PRT_TAB) " " "\033[1;34m[\033[1;35m$@\033[1;34m]\033[0m\n"
 	@printf $(PRT_TAB) " " ""
-	@printf $(PRT_TAB) " " ""
 	@$(MAKE) --no-print-directory -C $@ $(MAKECMDGOALS)
+#	@printf $(PRT_TAB) "\b" ""
+
 
 #UPDATE_URL = 'https://raw.githubusercontent.com/jmparis/Makefiles/master/Makefile-folders.mk'
 #include ./.mk/rules/rule-update.mk
+
 include ${MKMK_HOME}/Makefiles/rules/rule-DEFAULT-verbose.mk
