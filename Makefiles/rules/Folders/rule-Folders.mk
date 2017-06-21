ad96432a53f6c1d0140b04c6f48ab70fb537c92d
Jean-Michel Paris <jean_michel_paris@hotmail.fr>
2017-06-21 15:30:14 +0200

    Added help target

--- Makefiles/rules/Folders/rule-Folders.mk
+++ Makefiles/rules/Folders/rule-Folders.mk
@@ -1,10 +1,12 @@
-TOPTARGETS	:=	all clean clobber install uninstall run help update
+TOPTARGETS	:=	clean clobber install uninstall run update
 SUBDIRS		:=	$(sort $(wildcard */.))
 
 PRT_TAB		:=	%$(shell echo \($(MAKELEVEL) + 1 \) \* 2 | bc)b%b
 
-.PHONY:	$(TOPTARGETS) $(SUBDIRS)
+.PHONY:	$(TOPTARGETS) help $(SUBDIRS)
+
 $(TOPTARGETS):	$(SUBDIRS)
+
 $(SUBDIRS):
 	@printf "\r"
 	@printf $(PRT_TAB) " " "\033[1;34m[\033[1;35m$@\033[1;34m]\033[0m\n"
@@ -12,6 +14,15 @@ $(SUBDIRS):
 	@$(MAKE) --no-print-directory -C $@ $(MAKECMDGOALS)
 #	@printf $(PRT_TAB) "\b" ""
 
+help:	folders_man	$(SUBDIRS)
+
+folders_man:
+	@printf "\r"
+	@printf $(PRT_TAB) " " "The supported targets are:\n"
+	@printf $(PRT_TAB) " " "make............: to compile all subprojects\n"
+	@printf $(PRT_TAB) " " "make clean......: to clean all subprojects\n"
+	@printf $(PRT_TAB) " " "make install....: to install subprojects\n"
+	@printf $(PRT_TAB) " " "make uninstall..: to remove subprojects from system\n"
 
 #UPDATE_URL = 'https://raw.githubusercontent.com/jmparis/Makefiles/master/Makefile-folders.mk'
 #include ./.mk/rules/rule-update.mk
